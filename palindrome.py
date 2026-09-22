4e81904b78cb9a1c483b91e63a99f3d3b50ee8b1
ankitagour785@gmail.com <ankitagour785@gmail.com>
2026-09-22 16:57:17 +0530

    commit 2

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+
+def palindrome_number(n):
+    res = 0
+    num = n
+    while num > 0:
+        last = num % 10
+        res = (res * 10 ) + last
+        num //= 10
+    return n == res
+
+n = 567435
+print(palindrome_number(n))    
